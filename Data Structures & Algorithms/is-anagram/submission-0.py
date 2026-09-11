4745056133b8b1ef1263b3e51f095db841521931
Abin Kuzhuvelikalam Binoy <abinbinoy1134@gmail.com>
2026-09-10 19:10:27 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d = {}
+        d2 = {}
+
+        for i in s:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+
+        for i in t:
+            if i in d2:
+                d2[i] += 1
+            else:
+                d2[i] = 1
+
+        return d == d2
+        
